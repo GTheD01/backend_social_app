eb036a9de2bb580e174ed06bcb4af1899a0a3889
GTheD01 <popeftimovgeorgi@yahoo.com>
2024-10-03 19:39:08 +0200

    User admin page modified

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import UserAccount, OTP
 
-# Register your models here.
 admin.site.register(UserAccount)
 admin.site.register(OTP)
+
+class UserAdmin(UserAdmin):
+    readonly_fields = ('date_joined',)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2')}
+         ),
+    )
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Groups', {'fields': ('groups',)}),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('email',)
